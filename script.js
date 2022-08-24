a078491e4fcebe41a9bd20fb5a2f63937126e4eb
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-08-24 16:52:28 +0700

    Update owner

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const username = 'chamodperera';
+const username = 'lutfailham96';
 const maxPages = 2;
 const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
